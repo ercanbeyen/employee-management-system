c063568f80d54a7af65bc1f34ae99a22fa3b0d4a
Ercan Beyen <96631352+ercanbeyen@users.noreply.github.com>
2022-09-30 21:48:04 +0300

    OpenAPI integration

--- src/main/java/com/ercanbeyen/springbootfirstrestapi/SpringBootFirstRestApiApplication.java
+++ src/main/java/com/ercanbeyen/springbootfirstrestapi/SpringBootFirstRestApiApplication.java
@@ -1,9 +1,15 @@
 package com.ercanbeyen.springbootfirstrestapi;
 
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "User API", version = "1.0", description = "User Information"))
 public class SpringBootFirstRestApiApplication {
 
 	public static void main(String[] args) {
